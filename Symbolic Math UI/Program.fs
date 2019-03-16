e1d589f824a86bf846201bb63cf05fd99e9d5ca1
Frank Lideros <flideros@hotmail.com>
2019-03-15 17:00:48 -0700

    Daily edits (#40)
    * Made fsharp,core stop copy local
    
    * Started adding defenitions to functions by promoting them to Modules
    
    * Refactor Set Module
    
    * Changed the file Set to Collection
    
    * Done refactoring for the night
    
    * created a get definition entry function
    
    * Tested and refactored Union, Difference, and Intersetion.
    
    * Destroyed the Universe (Type) and continued refactoring
    
    * Added open math functions to the function defenitions
    
    * done for the night
    
    * more refactoring
    
    * Nightly coding
    
    * Refactor continues
    
    * Done for the night
    
    * done for the night
    
    * Started modeling MathML
    
    * Still modeling MathML
    
    * Working on modeling MathML
    
    * Working on  default values for MathML Attributes.
    
    * Defining default atributes for MathML elements (up to <mo>)
    
    * I'm up to the table attributes.
    
    * Attribues for MathML are done
    
    * Starting to work on argument validation
    
    * Testing functions to send an expression an md file
    
    * Working on display functions
    
    * finished the string builder function for MathML tags
    
    * Workingon an operator dictionary
    
    * I put the OperatorDictionary functions in alphabetical order
    
    * Started working on UI
    
    * Symbolic Math 1 has to use FSharp.Core 4.5.2
    
    * Working on UI
    
    * WPF is fun
    
    * created a calculator control
    
    * secon wind
    
    * finished calculator control logic with only one muteable
    
    * Implemented more functions
    
    * fixed memory subtract on calculator
    
    * finished basic calculator
    
    * cleaned up the solution folders

---
+++ Symbolic Math UI/Program.fs
@@ -0,0 +1,54 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module App  
+   
+open System
+open System.Windows          
+open System.Windows.Controls  
+open System.Windows.Media
+open System.Windows.Shapes  
+open System.IO
+open System.Windows.Markup
+open System.Windows.Controls 
+open System.Reflection
+open System.Windows.Media.Imaging
+open Style
+open Control
+open BasicCalculator
+
+//Define app resources and load them to the main progream.
+let resource = new Uri("app.xaml",System.UriKind.Relative)
+let mainProgram = Application.LoadComponent(resource) :?> Application
+
+// Create Types
+let browser = FrameBrowser(@"https://www.bing.com/")
+let browserBorder = Border()
+let dockPanel = DockPanelStyle()
+let button = ButtonStyle("Frank's Button")
+
+// Compose Types
+browserBorder.Child <- browser
+
+dockPanel.Children.Add (button) |> ignore
+dockPanel.Children.Add (browserBorder) |> ignore
+
+
+let calculator = new Calculator(OverridesDefaultStyle = true) 
+
+// Make a window and add content
+let window = new Window()
+window.Title <- "F# is fun!" 
+window.Content <-  calculator
+window.SizeToContent <- SizeToContent.WidthAndHeight
+//----------{not needed unless a Xaml used for window}----------//
+// Load XAML -  XAML - MUST be Embedded Resource  ("use  {file name}.xaml")    
+//{not needed unless a Xaml used for window} let mutable this : Window = Utilities.contentAsXamlObject("MainWindow.xaml"):?> Window  
+
+[<STAThread>] 
+[<EntryPoint>]
+let main(_) =  
+    do mainProgram.Run(window) |> ignore
+    0
+    
+    
