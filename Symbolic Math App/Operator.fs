24150fb6ea02e0c25f459e5b2cbf382c3d947f81
Frank Lideros <flideros@hotmail.com>
2019-04-22 17:26:14 -0700

    trying to remove old folders

--- Symbolic Math App/Operator.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace UI
-
-module Operator =
-
-    /// This operator is similar to (|>). 
-    /// But, it returns argument as a return value.
-    /// Then you can chain functions which returns unit.
-    /// http://fssnip.net/9q
-    let ($) x f = f x ; x
