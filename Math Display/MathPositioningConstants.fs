141c522647ea6d778b02a9d89afc6ecebdf531d3
Frank Lideros <flideros@hotmail.com>
2020-03-28 19:46:17 -0700

    Added math positioning constants

---
+++ Math Display/MathPositioningConstants.fs
@@ -0,0 +1,239 @@
+﻿namespace Math.Presentation
+
+module MathPositioningConstants =
+    // ref. https://docs.microsoft.com/en-us/typography/opentype/spec/math
+    // Suggested values are from the microsoft documentation, the used
+    // values are from the STIX 2 Math font.
+    
+    /// Percentage of scaling down for level 1 superscripts and subscripts. 
+    /// Suggested value: 80%.
+    let scriptPercentScaleDown = 70.
+    
+    /// Percentage of scaling down for level 2 (scriptScript) superscripts and 
+    /// subscripts. Suggested value: 60%.
+    let scriptScriptPercentScaleDown = 55.
+    
+    /// Minimum height required for a delimited expression (contained within 
+    /// parentheses, etc.) to be treated as a sub-formula.      
+    /// Suggested value: normal line height × 1.5.
+    let delimitedSubFormulaMinHeight = 1325.
+    
+    /// Minimum height of n-ary operators (such as integral and summation) for 
+    /// formulas in display mode (that is, appearing as standalone page elements, 
+    /// not embedded inline within text).
+    let displayOperatorMinHeight = 1800.
+    
+    /// White space to be left between math formulas to ensure proper line spacing. 
+    /// For example, for applications that treat line gap as a part of line ascender, 
+    /// formulas with ink going above (os2.sTypoAscender + os2.sTypoLineGap - MathLeading) 
+    /// or with ink going below os2.sTypoDescender will result in increasing line height.
+    let mathLeading = 150.
+    
+    /// Axis height of the font. In math typesetting, the term axis refers to a 
+    /// horizontal reference line used for positioning elements in a formula. 
+    /// The math axis is similar to but distinct from the baseline for regular  
+    /// text layout.For example, in a simple equation, a minus symbol or fraction rule  
+    /// would be on the axis,but a string for a variable name would be set on a baseline  
+    /// that is offset from the axis. The axisHeight value determines the amount of that 
+    /// offset.
+    let axisHeight = 258.
+    
+    /// Maximum (ink) height of accent base that does not require raising the accents. 
+    ///Suggested: x‑height of the font (os2.sxHeight) plus any possible overshots.
+    let accentBaseHeight = 480.
+    
+    /// Maximum (ink) height of accent base that does not require flattening the accents. 
+    /// Suggested: cap height of the font (os2.sCapHeight).
+    let flattenedAccentBaseHeight = 656.
+    
+    /// The standard shift down applied to subscript elements. Positive for moving in 
+    /// the downward direction. Suggested: os2.ySubscriptYOffset.
+    let subscriptShiftDown = 210.
+    
+    /// Maximum allowed height of the (ink) top of subscripts that does not require 
+    /// moving subscripts further down. Suggested: 4/5 x- height.
+    let subscriptTopMax = 368.
+    
+    /// Minimum allowed drop of the baseline of subscripts relative to the (ink) bottom 
+    /// of the base. Checked for bases that are treated as a box or extended shape. 
+    /// Positive for subscript baseline dropped below the base bottom.
+    let subscriptBaselineDropMin = 160.
+    
+    /// Standard shift up applied to superscript elements. 
+    /// Suggested: os2.ySuperscriptYOffset.
+    let superscriptShiftUp = 360.
+    
+    /// Standard shift of superscripts relative to the base, in cramped style.
+    let superscriptShiftUpCramped = 252.
+    
+    /// Minimum allowed height of the (ink) bottom of superscripts that does not require 
+    /// moving subscripts further up. Suggested: ¼ x-height.
+    let superscriptBottomMin = 120.
+    
+    /// Maximum allowed drop of the baseline of superscripts relative to the (ink) top 
+    /// of the base. Checked for bases that are treated as a box or extended shape. 
+    /// Positive for superscript baseline below the base top.
+    let superscriptBaselineDropMax = 230.
+    
+    /// Minimum gap between the superscript and subscript ink. 
+    /// Suggested: 4 × default rule thickness.
+    let subSuperscriptGapMin = 150.
+    
+    /// The maximum level to which the (ink) bottom of superscript can be pushed to 
+    /// increase the gap between superscript and subscript, before subscript starts 
+    /// being moved down. Suggested: 4/5 x-height.
+    let superscriptBottomMaxWithSubscript = 380.
+    
+    /// Extra white space to be added after each subscript and superscript. 
+    /// Suggested: 0.5 pt for a 12 pt font.
+    let spaceAfterScript = 40.
+    
+    /// Minimum gap between the (ink) bottom of the upper limit, and the (ink) 
+    /// top of the base operator.
+    let upperLimitGapMin = 135.
+    
+    /// Minimum distance between baseline of upper limit and 
+    /// (ink) top of the base operator.
+    let upperLimitBaselineRiseMin = 300.
+    
+    /// Minimum gap between (ink) top of the lower limit, and 
+    /// (ink) bottom of the base operator.
+    let lowerLimitGapMin = 135.
+    
+    /// Minimum distance between baseline of the lower limit and 
+    /// (ink) bottom of the base operator.
+    let lowerLimitBaselineDropMin = 670.
+    
+    /// Standard shift up applied to the top element of a stack.
+    let stackTopShiftUp = 470.
+    
+    /// Standard shift up applied to the top element of a stack in display style.
+    let stackTopDisplayStyleShiftUp = 780.
+    
+    /// Standard shift down applied to the bottom element of a stack. Positive for 
+    /// moving in the downward direction.
+    let stackBottomShiftDown = 385.
+    
+    /// Standard shift down applied to the bottom element of a stack in display style. 
+    /// Positive for moving in the downward direction.
+    let stackBottomDisplayStyleShiftDown = 690.
+    
+    /// Minimum gap between (ink) bottom of the top element of a stack, and 
+    /// the (ink) top of the bottom element. Suggested: 3 × default rule thickness.
+    let stackGapMin = 150.
+    
+    /// Minimum gap between (ink) bottom of the top element of a stack, and 
+    /// the (ink) top of the bottom element in display style. 
+    /// Suggested: 7 × default rule thickness.
+    let stackDisplayStyleGapMin = 300.
+    
+    /// Standard shift up applied to the top element of the stretch stack.
+    let stretchStackTopShiftUp = 800.
+    
+    /// Standard shift down applied to the bottom element of the stretch stack. 
+    /// Positive for moving in the downward direction.
+    let stretchStackBottomShiftDown = 590.
+    
+    /// Minimum gap between the ink of the stretched element, and 
+    /// the (ink) bottom of the element above. Suggested: same value as upperLimitGapMin.
+    let stretchStackGapAboveMin = 68.
+    
+    /// Minimum gap between the ink of the stretched element, and the (ink) top of 
+    /// the element below. Suggested: same value as lowerLimitGapMin.
+    let stretchStackGapBelowMin = 68.
+    
+    /// Standard shift up applied to the numerator.
+    let fractionNumeratorShiftUp = 585.
+    
+    /// Standard shift up applied to the numerator in display style. 
+    /// Suggested: same value as stackTopDisplayStyleShiftUp.
+    let fractionNumeratorDisplayStyleShiftUp = 640.
+    
+    /// Standard shift down applied to the denominator. Positive for moving in 
+    /// the downward direction.
+    let fractionDenominatorShiftDown = 585.
+    
+    /// Standard shift down applied to the denominator in display style. Positive for 
+    /// moving in the downward direction. 
+    /// Suggested: same value as stackBottomDisplayStyleShiftDown.
+    let fractionDenominatorDisplayStyleShiftDown = 640.
+    
+    /// Minimum tolerated gap between the (ink) bottom of 
+    /// the numerator and the ink of the fraction bar. 
+    /// Suggested: default rule thickness.
+    let fractionNumeratorGapMin = 68.
+    
+    /// Minimum tolerated gap between the (ink) bottom of the numerator and the ink of 
+    /// the fraction bar in display style. Suggested: 3 × default rule thickness.
+    let fractionNumeratorDisplayStyleGapMin = 150.
+    
+    /// Thickness of the fraction bar. Suggested: default rule thickness.
+    let fractionRuleThickness = 68.
+    
+    /// Minimum tolerated gap between the (ink) top of the denominator and the ink of 
+    /// the fraction bar. Suggested: default rule thickness.
+    let fractionDenominatorGapMin = 68.
+    
+    /// Minimum tolerated gap between the (ink) top of the denominator and the ink of 
+    /// the fraction bar in display style. Suggested: 3 × default rule thickness.
+    let fractionDenominatorDisplayStyleGapMin = 150.
+    
+    /// Horizontal distance between the top and bottom elements of a skewed fraction.
+    let skewedFractionHorizontalGap = 350.
+    
+    /// Vertical distance between the ink of the top and bottom elements of 
+    /// a skewed fraction.
+    let skewedFractionVerticalGap = 68.
+    
+    /// Distance between the overbar and the (ink) top of he base. 
+    /// Suggested: 3 × default rule thickness.
+    let overbarVerticalGap = 175.
+    
+    /// Thickness of overbar. Suggested: default rule thickness.
+    let overbarRuleThickness = 68.
+    
+    /// Extra white space reserved above the overbar. 
+    /// Suggested: default rule thickness.
+    let overbarExtraAscender = 68.
+    
+    /// Distance between underbar and (ink) bottom of the base. 
+    /// Suggested: 3 × default rule thickness.
+    let underbarVerticalGap = 175.
+    
+    /// Thickness of underbar. Suggested: default rule thickness.
+    let underbarRuleThickness = 68.
+    
+    /// Extra white space reserved below the underbar. Always positive. 
+    /// Suggested: default rule thickness.
+    let underbarExtraDescender = 68.
+    
+    /// Space between the (ink) top of the expression and the bar over it. 
+    /// Suggested: 1¼ default rule thickness.
+    let radicalVerticalGap = 85.
+    
+    /// Space between the (ink) top of the expression and the bar over it. 
+    /// Suggested: default rule thickness + ¼ x-height.
+    let radicalDisplayStyleVerticalGap = 170.
+    
+    /// Thickness of the radical rule. This is the thickness of 
+    /// the rule in designed or constructed radical signs. 
+    /// Suggested: default rule thickness.
+    let radicalRuleThickness = 68.
+    
+    /// Extra white space reserved above the radical. 
+    /// Suggested: same value as radicalRuleThickness.
+    let radicalExtraAscender = 78.
+    
+    /// Extra horizontal kern before the degree of a radical, if such is present.
+    let radicalKernBeforeDegree = 65.
+    
+    /// Negative kern after the degree of a radical, if such is present. 
+    /// Suggested: −10/18 of em.
+    let radicalKernAfterDegree = -335.
+    
+    /// Height of the bottom of the radical degree, if such is present, 
+    /// in proportion to the ascender of the radical sign. Suggested: 60%.
+    let radicalDegreeBottomRaisePercent = 55.
+    
+    /// Minimum overlap of connecting glyphs during glyph construction, in design units.
+    let minConnectorOverlap = 100.
--- Math Display/TypeSetting.fs
+++ Math Display/TypeSetting.fs
@@ -30,17 +30,7 @@ type Glyph =
      string:string
      font:Font
      }
-
-type ControlSequence =
-    | ControlSymbol 
-    | ControlWord
-
-type Token =
-    | Char of TokenElement * Glyph
-    | ControlSequence of ControlSequence
-
 type GlyphBuilder = string -> Font -> Glyph
-
 type GlyphBox (glyph) as glyphBox =
     inherit Border(BorderThickness=Thickness(1.5),BorderBrush=Brushes.Red)
     let g = Grid()
@@ -90,14 +80,58 @@ type GlyphBox (glyph) as glyphBox =
         glyphBox.SetValue(Grid.RowSpanProperty,3)
         glyphBox.Child <- g
 
+
+type MathStyle =
+    (*
+      • Superscripts and subscripts are taken from the next smaller font unless in display mode they are taken in script font.
+      • Subscripts are always in the cramped variant. 
+      • Superscripts are only cramped if the original style was cramped.
+      • In a fraction, the numerator and denominator are taken from the next smaller font.
+      • The denominator is always in cramped variant
+      • The numerator is only in the cramped variant if the original style is cramped.
+      • Formulas under a radical or overline are in cramped variant.
+    *)
+    | Display
+    | Text
+    | Script
+    | ScriptScript
+    | DisplayCramped
+    | TextCramped
+    | ScriptCramped
+    | ScriptScriptCramped
+type MathObjectClassification =   
+    | Ordinary  // Lowercase Greek characters and those symbols that are just symbols.
+    | LargeOperator // Integral and sum signs, and big objects such as big cup for example.
+    | BinaryOperator
+    | BinaryRelation
+    | OpeningSymbol
+    | ClosingSymbol
+    | Punctuation
+    | Variable // Uppercase Greek letters and ordinary letters and digits.
+    | InnerSubformula
+
+type Token =
+    | Char of TokenElement * Glyph
+    | ControlSequence 
+
+
+
 module TypeSetting = 
     open MathML
     open MathML.OperatorDictionary
 
     // Font Sizes
-    let mathX00px = {emSquare = 1000.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 400.<MathML.px>}
+    let mathX00px = {emSquare = 1000.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 100.<MathML.px>}
+    let math30px = {emSquare = 700.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 100.<MathML.px>}
     let math200px = {emSquare = 1000.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 200.<MathML.px>}
     
+    let textSizeFont = {emSquare = 1000.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 200.<MathML.px>}
+    let scriptSizeFont = {emSquare = 700.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 200.<MathML.px>}
+    let scriptScriptSizeFont = {emSquare = 500.<MathML.em>; typeFace = Text.STIX2Math_Typeface; size = 200.<MathML.px>}
+    
+    
+
+
     let formatTextWithFont = fun t font   ->  Text.format t font.typeFace font.emSquare
     
     let getGlyph :GlyphBuilder = 
@@ -159,7 +193,7 @@ module TypeSetting =
         loop operator.character
 
     let getStringAtUnicode u = (char u).ToString()
-
+    
     let scaleGlyphBox (glyphBox :GlyphBox) (s:Size) = 
         do glyphBox.RenderTransform <- ScaleTransform(ScaleX = s.scaleX,ScaleY = s.scaleY)
 
@@ -188,33 +222,33 @@ module TypeSetting =
             gb
     
     let makeLineFrom (glyphs:Glyph list) =
-            let g = Grid()
-            let row0 = RowDefinition(Height = GridLength.Auto)
-            let row1 = RowDefinition(Height = GridLength.Auto)
-            do  g.RowDefinitions.Add(row0)
-                g.RowDefinitions.Add(row1)
+        let g = Grid()
+        let row0 = RowDefinition(Height = GridLength.Auto)
+        let row1 = RowDefinition(Height = GridLength.Auto)
+        do  g.RowDefinitions.Add(row0)
+            g.RowDefinitions.Add(row1)
             
-            let kerns = 
-            // compare adjacent glyph widths to the typeset width of the pair.
-                let leftGlyphs = List.truncate (glyphs.Length-1) glyphs
-                let rightGlyphs = glyphs.Tail
-                let glyphPairs = List.zip leftGlyphs rightGlyphs
-                let kerns = List.scan (fun acc (l,r) -> acc + (getHorizontalKern l r)) 0. glyphPairs
-                kerns
-            let positions = 
-                let initialPosition = {x=0.;y=0.}
+        let kerns = 
+        // compare adjacent glyph widths to the typeset width of the pair.
+            let leftGlyphs = List.truncate (glyphs.Length-1) glyphs
+            let rightGlyphs = glyphs.Tail
+            let glyphPairs = List.zip leftGlyphs rightGlyphs
+            let kerns = List.scan (fun acc (l,r) -> acc + (getHorizontalKern l r)) 0. glyphPairs
+            kerns
+        let positions = 
+            let initialPosition = {x=0.;y=0.}
                 
-                let _positions = 
-                    let p = List.scan (fun acc (x : Glyph) -> {x = (acc.x + x.width); y = 0.}) initialPosition glyphs
-                    List.truncate glyphs.Length p
+            let _positions = 
+                let p = List.scan (fun acc (x : Glyph) -> {x = (acc.x + x.width); y = 0.}) initialPosition glyphs
+                List.truncate glyphs.Length p
                 
-                let positionsWithKernApplied = 
-                    List.mapi (fun i (p:Position) -> {x = p.x + kerns.[i]; y = p.y}) _positions
+            let positionsWithKernApplied = 
+                List.mapi (fun i (p:Position) -> {x = p.x + kerns.[i]; y = p.y}) _positions
 
-                positionsWithKernApplied
-            let glyphBoxes = List.map2 (fun g p -> getGlyphBox g p) glyphs positions
-            do  List.iter (fun x -> g.Children.Add(x) |> ignore) glyphBoxes
-            g
+            positionsWithKernApplied
+        let glyphBoxes = List.map2 (fun g p -> getGlyphBox g p) glyphs positions
+        do  List.iter (fun x -> g.Children.Add(x) |> ignore) glyphBoxes
+        g
     
     (*Test Area*)
     type TestCanvas() as this  =  
@@ -222,35 +256,35 @@ module TypeSetting =
         
         let c0=  getGlyph (getOperatorString OperatorDictionary.cubeRootPrefix) mathX00px
         let c1 = getGlyph (getOperatorString mathematicalLeftFlattenedParenthesisPrefix) mathX00px
-        let c2 = getGlyph MathematicalStandardizedVariants.emptySet mathX00px
-        let c3 = getGlyph (MathematicalAlphanumericSymbols.LatinSerif.Normal.W) mathX00px
-        let c4 = getGlyph (MathematicalAlphanumericSymbols.LatinSerif.Normal.A) mathX00px
+        let c2 = getGlyph (MathematicalAlphanumericSymbols.Digit.Normal.two) mathX00px
+        let c3 = getGlyph (getOperatorString plusSignInfix) mathX00px
+        let c4 = getGlyph (MathematicalAlphanumericSymbols.LatinSerif.Italic.x) mathX00px
         let c5 = getGlyph (getOperatorString mathematicalRightFlattenedParenthesisPostfix) mathX00px
 
-        let glyphs = [c0;c1;c2;c3;c4;c5]
-        //let glyphs = [c3;c4]  
+        let glyphs1 = [c0;c1;c2;c3;c4;c5]
+        let line1 = makeLineFrom glyphs1
+        let line1_Width = List.fold (fun acc x -> x.width + acc) 0. glyphs1
 
-        let line = makeLineFrom glyphs
+        let s0=  getGlyph (getOperatorString OperatorDictionary.cubeRootPrefix) math30px
+        let s1 = getGlyph (getOperatorString mathematicalLeftFlattenedParenthesisPrefix) math30px
+        let s2 = getGlyph (MathematicalAlphanumericSymbols.Digit.Normal.two) math30px
+        let s3 = getGlyph (getOperatorString plusSignInfix) math30px
+        let s4 = getGlyph (MathematicalAlphanumericSymbols.LatinSerif.Italic.x) math30px
+        let s5 = getGlyph (getOperatorString mathematicalRightFlattenedParenthesisPostfix) math30px
+
+        let glyphs2 = [s2]//s0;s1;s2;s3;s4;s5]
+        let line2 = makeLineFrom glyphs2
         
-        let kerns = 
-            // compare adjacent glyph widths to the typeset width of the pair.
-                let leftGlyphs = List.truncate (glyphs.Length-1) glyphs
-                let rightGlyphs = glyphs.Tail
-                let glyphPairs = List.zip leftGlyphs rightGlyphs
-                let kerns = List.scan (fun acc (l,r) -> acc + (getHorizontalKern l r)) 0. glyphPairs
-                kerns
-
+       
         let textBlock =                    
             let tb = TextBlock()
-            tb.Text <- kerns.ToString()//(MathematicalAlphanumericSymbols.LatinSerif.Normal.W) //"FA"//"\U0001D49C"//"\ue0f2" 
+            tb.Text <- line1_Width .ToString()
             tb.FontStyle <- FontStyles.Normal
             tb.FontSize <- 60.
             tb.FontFamily <- Text.STIX2Math_FontFamily
             tb.Typography.StylisticSet1 <- true
             tb
-
         let canvas = Canvas(ClipToBounds = true)
-    
         let scale_Slider =
             let s = 
                 Slider(
@@ -263,10 +297,10 @@ module TypeSetting =
                     IsEnabled = true)        
             do s.SetValue(Grid.RowProperty, 0)
             let handleValueChanged (s) = 
-                line.RenderTransform <- 
+                line1.RenderTransform <- 
                     let tranforms = TransformGroup()
                     do  tranforms.Children.Add(TranslateTransform(X = 100., Y = 100.))
-                        tranforms.Children.Add(ScaleTransform(ScaleX = 5.0/s,ScaleY = 5.0/s))            
+                        tranforms.Children.Add(ScaleTransform(ScaleX = 15.0/s,ScaleY = 15.0/s))            
                     tranforms
                     
             s.ValueChanged.AddHandler(RoutedPropertyChangedEventHandler(fun _ e -> handleValueChanged (e.NewValue)))
@@ -276,8 +310,7 @@ module TypeSetting =
             do 
                 g.SetValue(DockPanel.DockProperty,Dock.Top)
                 g.Children.Add(scale_Slider) |> ignore
-            g
-    
+            g 
         let canvas_DockPanel =
             let d = DockPanel()
             do d.Children.Add(scaleSlider_Grid) |> ignore
@@ -289,8 +322,14 @@ module TypeSetting =
             do g.Children.Add(canvas_DockPanel) |> ignore        
             g
 
-        do  line.RenderTransform <- TranslateTransform(X = 0., Y = 200.)
-            canvas.Children.Add(line) |> ignore
+        do  line1.RenderTransform <- TranslateTransform(X = 0., Y = 200.)
+            line2.RenderTransform <- TranslateTransform(X = 317.8, Y = -30.)
+            line2.SetValue(Grid.RowProperty,1)
+
+            line1.Children.Add(line2) |> ignore
+
+            canvas.Children.Add(line1) |> ignore
+            //canvas.Children.Add(line2) |> ignore
             canvas.Children.Add(textBlock) |> ignore
             
             this.Content <- screen_Grid
